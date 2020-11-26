1ade9f9977b8183c8d7b54b43a161a487c44e557
Denys-FX <smartfox09@gmail.com>
2020-11-27 01:15:36 +0200

    Done tasks 1,2

---
+++ js/script-07-01.js
@@ -0,0 +1,11 @@
+const categoryList = document.getElementById('categories');
+const categoryTotal = categoryList.children.length;
+console.log(`В списке ${categoryTotal} категории.`);
+
+const itemList = document.querySelectorAll('.item');
+console.log(itemList);
+itemList.forEach(item => {
+    console.log(`Категория: ${item.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${item.lastElementChild.children.length}`);
+    }
+)
---
+++ js/script-07-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+
+const createListItem = (ingredient) => {
+    const elemContainer = document.createElement('li');
+    const elemTitle = document.createElement('h2')
+    elemTitle.textContent = ingredient;
+    elemContainer.append(elemTitle);
+    return elemContainer;
+}
+
+const listItems = ingredients.map(ingredient => createListItem(ingredient));
+console.log(listItems[0]);
+
+const ingredientsList = document.getElementById('ingredients');
+console.log(ingredientsList);
+
+ingredientsList.append(...listItems);
